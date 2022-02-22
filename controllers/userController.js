b8961f9871afb7ca350ca3dc3bda2f6a2e013d5f
Charlygaf <charlygaf@gmail.com>
2022-02-22 15:17:36 -0300

    ruta home sin logear

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,13 @@
+async function index(req, res) {
+  res.render("home");
+}
 
+module.exports = {
+  index,
+  show,
+  create,
+  store,
+  edit,
+  update,
+  destroy,
+};
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,22 +2,9 @@ const express = require("express");
 const publicRouter = express.Router();
 const userController = require("../controllers/userController");
 // Rutas del Públicas:
-// ...
 
 /* const userController = require("./userController"); */
 
-publicRouter.get("/usuarios", userController.index);
-
-publicRouter.get("/usuarios/crear", userController.showNewUserForm);
-
-publicRouter.get("/usuarios/crear/:id", userController.showCreate);
-
-publicRouter.post("/usuarios", userController.store);
-
-publicRouter.get("/usuarios/editar/:id", userController.Showedit);
-
-publicRouter.post("/usuarios/editar/:id", userController.update);
-
-publicRouter.delete("/usuarios/:id", userController.destroy);
+publicRouter.get("/", userController.index);
 
 module.exports = publicRouter;
