73f4c5c3275927d70615e0cf209868db83fb82f0
Valentina Giusti <valentinamgiusti@gmail.com>
2022-02-23 16:09:01 -0300

    passport

--- passport.js
+++ passport.js
@@ -1,60 +1,46 @@
-/* const passport = require("passport");
+const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const { User } = require("./models");
+const User = require("./schemas/User");
+const bcryptjs = require("bcryptjs");
 
 module.exports = (app) => {
   app.use(passport.session());
+
   passport.use(
-    new LocalStrategy({ usernameField: "email", passwordField: "password" }, async function verify(
-      username,
-      password,
-      cb,
-    ) {
-      try {
-        const user = await User.findOne({
-          where: {
-            email: username,
-          },
-        });
-        console.log(user);
-        if (!user) {
-          return cb(null, false, { message: "Incorrect email or password." });
+    new LocalStrategy(
+      { usernameField: "email", passwordField: "password", passReqToCallback: true, session: true },
+      async function verify(req, email, password, cb) {
+        try {
+          const user = await User.findOne({ email: email });
+          const hashedPassword = user.password;
+          const checkPassword = await bcryptjs.compare(password, hashedPassword);
+          if (!user) {
+            return cb(null, false, { message: "Incorrect email or password." });
+          }
+
+          if (!checkPassword) {
+            return cb(null, false, { message: "Incorrect email or password." });
+          }
+
+          return cb(null, user);
+        } catch (error) {
+          return cb(error);
         }
-        const result = await user.validPassword(password);
-        console.log(result);
-        if (!(await user.validPassword(password))) {
-          return cb(null, false, { message: "Incorrect email or  password." });
-        }
-        return cb(null, user);
-      } catch (error) {
-        return cb(error);
-      } */
+      },
+    ),
+  );
 
-// db.get('SELECT rowid AS id, * FROM users WHERE username = ?', [ username ], function(err, row) {
-//   if (err) { return cb(err); }
-//   if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+  passport.serializeUser(function (user, done) {
+    done(null, user.id); //id de usuario en session al login
+  });
 
-//   crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-//     if (err) { return cb(err); }
-//     if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-//       return cb(null, false, { message: 'Incorrect username or password.' });
-//     }
-//     return cb(null, row);
-//   });
-// });
-//     }),
-//   );
-//   passport.serializeUser(function (user, done) {
-//     done(null, user.id);
-//   });
-
-//   passport.deserializeUser(function (id, done) {
-//     User.findByPk(id)
-//       .then((user) => {
-//         done(null, user); // Usuario queda disponible en req.user.
-//       })
-//       .catch((error) => {
-//         done(error, user);
-//       });
-//   });
-// };
+  passport.deserializeUser(function (id, done) {
+    User.findById(id) //busca en bd a la persona que esta logueada (cada vez que se hace call a server)
+      .then((user) => {
+        done(null, user); // Usuario queda disponible en req.user.
+      })
+      .catch((error) => {
+        done(error, user);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const session = require("express-session");
 const express = require("express");
 const passport = require("./passport");
 const routes = require("./routes");
@@ -9,18 +10,19 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
+const MongoStore = require("connect-mongo");
 
-/* app.use(
+app.use(
   session({
-    secret: process.env.PASSPORT_SECRET,
-    store: new SequelizeStore({
-      db: sequelize,
+    secret: "holaaa",
+    saveUninitialized: false, // don't create session until something stored
+    resave: false, //don't save session if unmodified
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_CONNECTION_STRING,
     }),
-    resave: false,
-    saveUninitialized: false,
   }),
 );
-  */
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -35,7 +37,7 @@ mongoose.connection
   .once("open", () => console.log("¡Conexión con la base de datos establecida!"))
   .on("error", (error) => console.log(error));
 
-// dbInitialSetup();
+//dbInitialSetup();
 
 app.listen(APP_PORT, () =>
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}!\n`),
