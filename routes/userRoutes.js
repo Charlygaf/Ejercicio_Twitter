42f2f31fb8ea021740ad7d2bf4f5a57bbc178ed6
facuRodLop <facundo.rod99@gmail.com>
2022-02-24 09:07:23 -0300

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,10 @@ const userRouter = express.Router();
 userRouter.get("/home", userController.index);
 
 userRouter.post("/login", userController.logIn);
-userRouter.get("/:username", userController.show);
 
 userRouter.get("/registro", userController.create);
 userRouter.post("/registro", userController.store);
+userRouter.get("/:username", userController.show);
 userRouter.post("/:username", userController.following);
 
 module.exports = userRouter;
